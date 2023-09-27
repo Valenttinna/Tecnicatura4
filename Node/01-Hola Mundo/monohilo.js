82985a1726df99d3167ab0d7c6b0fee79ca4da1c
Valenttinna <111472360+Valenttinna@users.noreply.github.com>
2023-09-27 10:54:54 -0300

    Hola Mundo en node

---
+++ Node/01-Hola Mundo/monohilo.js
@@ -0,0 +1,14 @@
+console.log('Hola a todos')
+
+var i = 0;
+setInterval(function(){
+    console.log(i);
+    i++;
+    //if(i ===5){
+      //  console.log('Forzamos un eror')
+        //var a = 3 +z;
+    //}
+}, 1000);
+console.log('Segunda instruccion');
+
+//ctrl + c :para cortar la ejecucion

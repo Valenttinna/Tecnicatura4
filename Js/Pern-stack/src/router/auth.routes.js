505e7eb758bc4444b7545cd9dd7125913fc16da7
Valenttinna <111472360+Valenttinna@users.noreply.github.com>
2023-10-23 20:32:57 -0300

    C7-V7...Token terminado.

--- Js/Pern-stack/src/router/auth.routes.js
+++ Js/Pern-stack/src/router/auth.routes.js
@@ -1,11 +1,12 @@
 import Router  from "express-promise-router";
 import { profile, signin, signout, signup } from "../controllers/auth.controller.js";
+import { isAuth } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/signin", signin);
 router.post("/signup", signup);
 router.post("/signout", signout);
-router.get("/profile", profile);
+router.get("/profile",isAuth, profile);
 
 export default router;

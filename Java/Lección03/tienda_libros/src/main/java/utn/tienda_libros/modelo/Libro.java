8338a5c3c4524615f521530b12e7aee2bc1117cb
Valenttinna <111472360+Valenttinna@users.noreply.github.com>
2023-10-27 10:36:40 -0300

    Completo Tienda-Libros

---
+++ Java/Lección03/tienda_libros/src/main/java/utn/tienda_libros/modelo/Libro.java
@@ -0,0 +1,25 @@
+package utn.tienda_libros.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity //Porque es nuestra clase entidad
+@Data //
+@NoArgsConstructor //Constructor sin argumentos
+@AllArgsConstructor // Constructor con todos los argumentos
+@ToString
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idLibro;
+    String nombreLibro;
+    String autor;
+    Double precio;
+    Integer existencias;
+}

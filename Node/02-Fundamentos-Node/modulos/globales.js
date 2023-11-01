4ac484fef53e5b63901a35519931e346d2303d43
Valenttinna <111472360+Valenttinna@users.noreply.github.com>
2023-11-01 19:25:06 -0300

    C12 -documentacion

---
+++ Node/02-Fundamentos-Node/modulos/globales.js
@@ -0,0 +1,18 @@
+/*
+// this === global = true
+// Mostrar algo en consola
+console.log();
+
+//Mensaje en forma de error
+console.error();
+
+//Ejecutar un codigo luego de un intervalo de tiempo
+setTimeout(()=>{});
+
+//Ejecutar un codigo cada intervalo de tiempo
+setInterval(()=>{});
+
+SetInmediate(()=>{});
+*/
+
+console.log(setInterval);

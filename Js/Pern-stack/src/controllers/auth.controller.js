4b1a72eda21dd572090cfa8dda61e2efeebc8053
Valenttinna <111472360+Valenttinna@users.noreply.github.com>
2023-11-04 17:47:02 -0300

    C8 Backend Version1 finalizada

--- Js/Pern-stack/src/controllers/auth.controller.js
+++ Js/Pern-stack/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import {pool} from "../db.js"
 import bcrypt from "bcrypt";
 import { createAccessToken } from "../libs/jwt.js";
+import md5 from "md5";
 
 export const signin = async (req, res) => {
   const { email, password} = req.body;
@@ -30,8 +31,11 @@ export const signup = async(req, res, next) => {
 
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
-    const result= await pool.query("INSERT INTO usuarios ( name, email, password) VALUES ($1,$2,$3) RETURNING *", [name, email, hashedPassword]);
+    md5(email);
+    const gravatar = "http://gravatar.com/avatar/" + md5(email);
+
+
+    const result= await pool.query("INSERT INTO usuarios ( name, email, password, gravatar) VALUES ($1,$2,$3, $4) RETURNING *", [name, email, hashedPassword, gravatar]);
 
     const token = await createAccessToken({id: result.rows[0].id});
     console.log(result);

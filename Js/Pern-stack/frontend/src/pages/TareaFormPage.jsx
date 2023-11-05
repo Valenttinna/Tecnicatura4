071c0d9bf6abad5d451a5f4eb39d18d648110162
Valenttinna <111472360+Valenttinna@users.noreply.github.com>
2023-11-04 21:04:51 -0300

    C8 2parte Registro en frontend

---
+++ Js/Pern-stack/frontend/src/pages/TareaFormPage.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function TareaFormPage() {
+  return (
+    <div>TareaFormPage</div>
+  )
+}
+
+export default TareaFormPage
